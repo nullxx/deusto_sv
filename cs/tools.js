78c4f9ac309566a00e03bc63ede629857524057c
nullx <mail@nullx.me>
2020-02-01 00:43:14 +0100

    Commit with errors

---
+++ cs/tools.js
@@ -0,0 +1,120 @@
+var request = require('request');
+var Cookie = require('request-cookies').Cookie;
+const Config = require('../config')
+module.exports.getCookie = () => {
+    return new Promise((s, f) => {
+        var options = {
+            'method': 'GET',
+            'url': Config.baseUrl,
+            'headers': {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0',
+                'Accept': '*/*',
+                'Cache-Control': 'no-cache',
+                'Host': 'gaude.deusto.es',
+                'Accept-Encoding': 'gzip, deflate',
+                'Connection': 'keep-alive'
+            }
+        };
+        request(options, function (error, response) {
+            if (error) throw new Error(error);
+            var rawcookies = response.headers['set-cookie'];
+            var required = null
+            for (var i in rawcookies) {
+                var cookie = new Cookie(rawcookies[i]);
+                if (cookie.key == 'JSESSIONID') {
+                    required = cookie.value
+                }
+            }
+            if (required != null) {
+                s(required)
+                Config.sessionCookie = required
+            } else {
+                f("Cookie is not found")
+            }
+        });
+    })
+}
+module.exports.loginRequest = (username, password) => {
+    return new Promise((s, f) => {
+        this.getCookie().then(cookie => {
+            var options = {
+                'method': 'POST',
+                'url': Config.loginUrl,
+                'headers': {
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                    'Accept-Language': 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Origin': Config.originUrl,
+                    'Connection': 'keep-alive',
+                    'Referer': Config.baseUrl,
+                    'Upgrade-Insecure-Requests': '1',
+                    'Cookie': 'JSESSIONID=' + cookie
+                },
+                form: {
+                    'idUsuario': username,
+                    'password': password
+                }
+            };
+
+            request(options, function (error, response) {
+                if (error) f(error);
+                s(response.body)
+            });
+        })
+    })
+}
+module.exports.getHTML = (url, referer) => {
+    return new Promise((s, f) => {
+        var options = {
+            'method': 'GET',
+            'url': url,
+            'headers': {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept-Language': 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Origin': Config.originUrl,
+                'Connection': 'keep-alive',
+                'Referer': referer,
+                'Upgrade-Insecure-Requests': '1',
+                'Cookie': 'JSESSIONID=' + Config.sessionCookie
+            }
+        };
+
+        request(options, function (error, response) {
+            if (error) f(error);
+            s(response.body)
+        });
+
+    })
+}
+module.exports.gtS = (string) => {
+    return new Promise((s, f) => {
+        let one = string.split("('")[1]
+        let two = one.split("')")[0]
+        if (two != null) {
+            s(two)
+        } else {
+            f("URL was not found")
+        }
+    })
+}
+module.exports.gtA = (htmlString) => {
+    return new Promise((s, f) => {
+        let one = htmlString.split('$("#frameApp").attr("src", "')[1]
+        let two = one.split('");')[0]
+        if (two != null){
+            s(two)
+        }else{
+            f('Error getting frameURL')
+        }
+
+    })
+}
+module.exports.parseText = (htmlString) => {
+    if (htmlString != null){
+        return htmlString.replace(/(\r\n|\n|\r)/gm,"").trim();
+
+    }
+}
